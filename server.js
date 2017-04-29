9443c9df1fb8794bffd861a4a2f1f54b3c31c17c
atsuydam <amanda.suydam@ucdenver.edu>
2017-04-29 09:49:21 -0600

    changed to using group Mlab
    now using group mlab so people can see their testing

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 // load the express package and create our app
 var express = require('express');
 var app 	= express();
@@ -12,13 +11,13 @@ var Recipe = require('./app/models/recipe');
 var User = require('./app/models/USER')
 var superSecret = 'iwishihadapersonalchef'; // for our jwt
 // connect to the database at mongolab.
- mongoose.connect('mongodb://AwesomeAmanda:dave4275@ds143980.mlab.com:43980/movies');
+mongoose.connect('mongodb://Amanda:WebAPI3800@ds117311.mlab.com:17311/pantry');
 
 // Test for a successful connection
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
-	console.log('connected successfully');
+    console.log('connected successfully');
 });
 
 //body parser
@@ -26,17 +25,17 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-	//not entirely confident on these since they are bring up undefined but from the book
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-	next();
+    //not entirely confident on these since they are bring up undefined but from the book
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    next();
 });
 //console logging
 app.use(morgan('dev'));
 
 // grab our index homepage
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 var apiRouter = express.Router();
@@ -50,19 +49,54 @@ apiRouter.route('/recipes')
             res.json(recipes);
         });
     });
-// return one recipe, no need to login *these haven't been changed, just moved around to put similar items together*
-apiRouter.route('recipes/:recipe_name')
-// deletes a recipe by title
-    .delete(function (req, res) {
-        Recipe.remove({
-            name: req.params.recipe_name
-        }, function(err, movie){
-            if (err) return res.send(err);
-            res.json({message: 'Successfully deleted by Name'});
-            res.json({message:'That is not your recipe to delete'})
-        });
+
+
+// *************************************************************************************************************
+//	Kelsey's section
+// *************************************************************************************************************
+
+// both of these search functions work in exactly the same way
+// these will search for a recipe/ingredient with any part of the search string matching
+// the searched for string should be located in the query parameters
+
+apiRouter.route('/search/recipes')
+    .get(function(req, res){
+        Recipe.find(
+            { "title": { $regex: req.query.name, $options: 'i' }  },
+            function(err, found){
+                if(err) return res.send(err);
+                res.json(found);
+            }
+        )
     });
-// returns by id but I'll be honest and not sure we need this. But it was in the original
+
+apiRouter.route('/search/ingredients')
+    .get(function(req, res){
+        Recipe.find(
+            { "ingredients": { $regex: req.query.name, $options: 'i' } },
+            function(err, found){
+                if(err) return res.send(err);
+                res.json(found);
+            }
+        )
+    });
+
+apiRouter.route('/search/recipes_by_user')
+    .get(function(req, res){
+        Recipe.find(
+            { "postedBy": req.query.name },
+            function(err, found){
+                if(err) return res.send(err);
+                res.json(found);
+            }
+        )
+    });
+
+
+// For testing purposes only - take out of final project ----------------------------------------------------
+
+// This is so I can add and test stuff without messing with being a user
+
 apiRouter.route('/recipes/:recipe_id')
 //returns the recipe by id
     .get(function(req, res){
@@ -73,7 +107,45 @@ apiRouter.route('/recipes/:recipe_id')
 
             res.json(recipes);
         }));
+    })
+
+    .delete(function (req, res) {
+        Recipe.remove({
+            _id: req.params.recipe_id
+        }, function(err, recipe){
+            if (err) return res.send(err);
+
+            res.json({message: 'Successfully deleted'});
+        });
     });
+
+// this is the same as the one in the users section - just a function here
+// so I can add a recipe without being a user
+apiRouter.route('/recipes')
+    .post(function(req, res, next){
+        var recipe = new Recipe({
+            title: req.body.title,
+            postedBy: "Dev",
+            ingredients: req.body.ingredient,
+            direction: req.body.direction
+        });
+        recipe.save(function(err){
+            if (err) res.json(err);
+            else res.json({ message: 'Recipe created!'});
+        });
+    });
+
+
+// For testing purposes only - take out of final project ----------------------------------------------------
+
+// Kelsey - deleted a block here that was a repeat of some code below
+
+// *************************************************************************************************************
+// End Kelsey's section
+// *************************************************************************************************************
+
+
+
 
 //*************************************************************************************************************
 // Amanda Added here
@@ -100,14 +172,14 @@ apiRouter.post('/users', (function(req, res) {
         res.json({message: 'User created!'})
     })
 }));
-    //returns all the users
+//returns all the users
 apiRouter.get('/users', function(req, res){
-        User.find(function(err, users){
-            if(err) res.send(err);
+    User.find(function(err, users){
+        if(err) res.send(err);
 
-            res.json(users);
-        });
+        res.json(users);
     });
+});
 
 // our authentication route that user will go through in order to access their page
 apiRouter.post('/login', function(req, res) {
@@ -135,31 +207,31 @@ apiRouter.use(function(req, res, next) {
     console.log('Someone is visiting');
     // route middleware to verify a token
     //apiRouter.use(function (req, res, next) {
-        // check header or url parameters or post parameters for token
-        var token = req.body.token || req.query.token || req.headers['x-access-token'];
-        // decode token
-        if (token) {
-            // verifies secret and checks exp
-            jwt.verify(token, superSecret, function (err, decoded) {
-                if (err) {
-                    return res.status(403).send({
-                        success: false,
-                        message: 'Failed to authenticate token.'
-                    });
-                } else {
-                    // if everything is good, save to request for use in other routes
-                    req.decoded = decoded;
-                    next();
-                }
-            });
-        } else {
-            // if there is no token
-            // return an HTTP response of 403 (access forbidden) and an error message
-            return res.status(403).send({
-                success: false,
-                message: 'No token provided.'
-            });
-        }
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, superSecret, function (err, decoded) {
+            if (err) {
+                return res.status(403).send({
+                    success: false,
+                    message: 'Failed to authenticate token.'
+                });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        // if there is no token
+        // return an HTTP response of 403 (access forbidden) and an error message
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
 });
 // these routes are register user only
 apiRouter.route('/user_recipes')
@@ -169,11 +241,10 @@ apiRouter.route('/user_recipes')
             title: req.body.title,
             postedBy: req.headers['username'],
             // Made some changes for testing, couldn't get ref to work so chaining username from login
-            ingredients: [{
-                in_name: req.body.ingredient,
-                measurement: req.body.measurement,
-                amount: req.body.amount
-            }],
+
+            // edited by kelsey
+            ingredients: req.body.ingredient,
+
             direction: req.body.direction
         });
         recipe.save(function(err){
@@ -192,18 +263,27 @@ apiRouter.route('/user_recipes')
 
         })
     })
-// update a user's recipe
+    // update a user's recipe
+
+    // this function needs some love
+    // this function would allow any logged-on user to edit any recipe
     .put(function (req, res) {
 
         // use the user model to find the user we want
+        // Kelsey - also this function allows any user to edit a recipe, not just the user that posted it - is that the intention?
         Recipe.findByTitle(req.params.recipe.name, function (err, recipe) {
 
             if (err) res.send(err);
             // update the recipe's info only if it's new
             if (req.body.name) recipe.name = req.body.name;
+            // changed this part because I took out the measurement part
+            // have not tested this part in postman
+            if (req.body.ingredients.ingredient) recipe.ingredient = req.body.ingredients.ingredient;
             if (req.body.ingredients.ingredient) recipe.in_name = req.body.ingredients.ingredient;
-            if (req.body.ingredients.measurement) recipe.measurement = req.body.ingredients.measurement;
-            if (req.body.ingredients.amount) recipe.amount = req.body.ingredients.amount;
+
+            // Note from Kelsey: removed this stuff
+            //if (req.body.ingredients.measurement) recipe.measurement = req.body.ingredients.measurement;
+            //if (req.body.ingredients.amount) recipe.amount = req.body.ingredients.amount;
 
             // save the recipe
             recipe.save(function (err) {
@@ -214,7 +294,8 @@ apiRouter.route('/user_recipes')
             });
         })
     })
-// User deletes a single recipe in the database
+    // User deletes a single recipe in the database
+    // kelsey - not sure what's going on with this function
     .delete(function (req, res) {
         Recipe.remove({
             // change body to however to it's passing but not param with the token. My first thought is list recipes
